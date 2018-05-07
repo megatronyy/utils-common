d340c35191de7b0c8eec6e602b35ead319730ab7
屈文斌 <twfx7758@hotmail.com>
2018-05-07 17:21:21 +0800

    RPC服务与客户端

---
+++ src/main/java/org/quwb/utils/netty/server/NettyServer.java
@@ -0,0 +1,17 @@
+package org.quwb.utils.netty.server;
+
+import java.io.IOException;
+
+/**
+ * @author quwb
+ * @create 2018-05-07 16:37
+ * @desc
+ **/
+public class NettyServer {
+
+    private static final int port = 50052;
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
