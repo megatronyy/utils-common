c347acaec647972017ddf0decd6c69690b38bac0
屈文斌 <twfx7758@hotmail.com>
2018-05-07 11:06:40 +0800

    Initial commit

---
+++ src/main/java/org/quwb/utils/es/EsMain.java
@@ -0,0 +1,13 @@
+package org.quwb.utils.es;
+
+/**
+ * @author quwb
+ * @create 2018-05-07 10:59
+ * @desc Es主函数
+ **/
+public class EsMain {
+    public static void main(String[] args) {
+
+
+    }
+}
